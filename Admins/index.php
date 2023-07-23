ce8fd19a238313e53950aa968c9269b5aacc2a2e
harshita-2003 <harshitag374@gmail.com>
2023-07-23 19:45:22 +0530

    started with backend

---
+++ Admins/index.php
@@ -0,0 +1,71 @@
+<?php
+ require('inc/db_config.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login Panel</title>
+    <link rel="stylesheet" href="css/common.css">
+
+    <?php require('inc/links.php'); ?>
+
+    <link rel="icon" href="images/logo.jpg">
+
+    <style>
+        div.login-form{
+            position: absolute;
+            top:50%;
+            left:50%;
+            transform: translate(-50%,-50%);
+            text-align: center;
+            width: 400px;
+        }
+        .custom-bg{
+            color:white;
+            background-color:mediumturquoise;
+        }
+        .custom-bg:hover{
+            background-color:mediumturquoise;
+            opacity: 80%;
+        }
+    </style>
+</head>
+<body class="bg-light">
+    
+    <div class="login-form rounded bg-white shadow overflow-hidden">
+        <form method="post">
+            <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
+            <div class="p-4">
+                <div class="mb-3">
+                    <input name="admin_name" required type="text" class="form-control shadow-none text-center" placeholder="Admin Name">
+                </div>
+                <div class="mb-4">
+                    <input name="admin_pass" required type="password" class="form-control shadow-none text-center" placeholder="Admin Password">
+                </div>
+                <button name="login" type="submit" class="btn text-white custom-bg shadow-none">LOGIN</button>
+            </div>
+        </form>
+    </div>
+
+
+<?php 
+    if(isset($_POST['login']))
+    {
+        $frm_data = filteration($_POST);
+
+        $query = "SELECT * FROM `admin_cred` WHERE `admin_name`=? AND `admin_pass` =? ";
+        $values = [$frm_data['admin_name'],$frm_data['admin_pass']];
+        $datatype = "ss";
+
+        $res = select($query,$values,$datatype);
+        print_r($res);
+    }
+?>
+
+
+    <?php require('inc/scripts.php') ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,22 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel paradise</title>
 
-    <!--google fonts-->
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@1,300;1,400&family=Montserrat:wght@300;400;500;600;700&family=Roboto&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
-
-    <!-- Css stylesheets -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
-
-    <!-- Favicon icon -->
-    <link rel="icon" href="images/logo.jpg">
-
-    <!-- Font awesome -->
-    <script src="https://kit.fontawesome.com/afb2b09bfd.js" crossorigin="anonymous"></script>
-
-    <!-- Bootstrap script -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <?php require('inc/links.php') ?>
 
     <!-- swiperjs -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
